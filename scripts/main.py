68795789e43b38d1bf1b121cb1cbac0145a602c0
Tania <161022301+TaniaGraff@users.noreply.github.com>
2024-07-01 11:41:18 +0200

    Retoques redacción archivo.

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,8 @@
 import soporte
 
-print('______________________FASE 1. EXPLORACIÓN INICIAL Y LIMPIEZA DE DATOS_______________________')
+print('_____________     FASE 1. EXPLORACIÓN INICIAL Y LIMPIEZA DE DATOS     __________________')
 
-print('_______________APERTURA CSV, EXPLORACIÓN Y LIMPIEZA DATAFRAME ACTIVIDAD CLIENTES____________')
+print('________  APERTURA CSV, EXPLORACIÓN Y LIMPIEZA DATAFRAME ACTIVIDAD CLIENTES   __________')
 
 #Llamo a la función de leer csv de la actividad de los clientes.
 df_actividad_clientes = soporte.leer_csv('../data/input_data/Customer Flight Activity.csv')
@@ -25,7 +25,7 @@ soporte.cambiar_tipo_float(df_actividad_clientes, 'Points Redeemed')
 #Llamo a la función para cambiar el tipo de la columna Month.
 soporte.cambiar_mes_a_nombre(df_actividad_clientes, 'Month')
 
-#Únicamente llamo a la función para ver los principales estadísticos de las columnas numéricas porque no tiene ninguna columna categórica.
+#Únicamente llamo a la función para ver los principales estadísticos de las columnas numéricas porque no tiene ninguna columna categórica, salvo Month.
 print('Los principales estadísticos de las COLUMNAS NUMÉRICAS del DataFrame de la ACTIVIDAD CLIENTES son:\n')
 estadisticos_numericos = soporte.estadisticos_numericos(df_actividad_clientes)
 print(estadisticos_numericos)
@@ -83,13 +83,13 @@ soporte.eliminar_columnas_nulos(df_historial_clientes, columnas_nulos_numericos)
 print(df_historial_clientes.columns)
 print('.....................................................')
 
-#Llamo a la función para cambiar el único valor negativo de la columna Salary, ya que al ser el único, entiendo que ha sido un error al imputar ese dato.
+#Llamo a la función para cambiar los valores negativos de la columna Salary, entendiendo que se han podido generar erróneamente durante el almacenamiento de los datos.
 print('Los valores únicos de la columna SALARY tras corregir los valores negativos son:\n')
 soporte.quitar_negativos(df_historial_clientes, 'Salary')
 print(df_historial_clientes['Salary'].value_counts())
 print('.....................................................')
 
-#Llamo a la función para calcular la media y la mediana de la columna Salary y ver a qué valor imputo los nulos.
+#Llamo a la función para calcular la media y la mediana de la columna Salary y ver a qué valor puedo imputar los nulos.
 media_mediana_salary= soporte.media_mediana(df_historial_clientes, 'Salary')
 print('.....................................................')
 
@@ -99,7 +99,7 @@ print('Los nulos en el DataFrame de HISTORIAL CLIENTES son:')
 print(df_historial_clientes.isnull().sum())
 
 
-print('___________________UNIÓN DATAFRAMES_________________')
+print('______________    UNIÓN DATAFRAMES    _______________')
 
 #Llamo a la función para unir los dos DF usando un merge left para quedarme con todos los registros de clientes aun cuando no tengan una actividad registrada.
 #De este modo se podrá analizar el comportamiento de todos ellos e identificar, en caso de que fuera necesario, porqué esos clientes inactivos no han registrado ninguna actividad.
@@ -123,11 +123,11 @@ print('Los duplicados del DataFrame CLIENTES son:')
 print(soporte.duplicados(df_clientes))
 print('.....................................................')
 
-#Llamo a la función para analizar los duplicados. (Decido mantenerlos, pues el mes de la transacción cambia)
+#Llamo a la función para analizar los duplicados. 
 duplicados= soporte.analisis_duplicados(df_clientes)
 print(duplicados)
 
-print('__________________________________OBSERVACIONES___________________________________')
+print('________________________      OBSERVACIONES     ________________________________')
 
 print("""El DataFrame CLIENTES resultante contiene información de los clientes canadienses
 que forman parte del programa de fidelización de la compañía aérea. Clientes de los que se 
@@ -144,7 +144,7 @@ descartarlos ya que nos faltan datos como el identificador único de compra, el 
 de la compra para poder hacerlo con mayor seguridad. Es por ello que el DataFrame CLIENTES 
 resultante tiene un total de 405624 registros y 22 columnas.""")
 
-print('__________________________ALMACENAMIENTO DATAFRAME CLIENTES________________________')
+print('_________________   ALMACENAMIENTO DATAFRAME CLIENTES   ________________________')
 
 #Llamo a la función para guardar el DF CLIENTES resultante en la carpeta output_data.
 soporte.guardar_df(df_clientes, 'customer_combined')
